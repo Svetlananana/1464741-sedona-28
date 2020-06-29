164ad3760e1a36bf394713656f433c6180b941c3
Svetlananana <hellosvetasmirnova@gmail.com>
2020-06-29 17:43:18 +0300

    Проверка 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 var byButton = document.querySelector('.button')
 var popup = document.querySelector('.hotel-class')
 var form = document.querySelector('.form-hotel-index')
@@ -14,20 +13,8 @@ byButton.addEventListener('click', function () {
 form.addEventListener('submit', function (evt) {
   if (!checkin.value || !checkout.value) {
     evt.preventDefault()
-    popup.classList.remove("input-error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("input-error");
+    form.classList.remove("input-error");
+    form.offsetWidth = popup.offsetWidth;
+    form.classList.add("input-error");
   }
 })  
-
-/*var byButton = document.querySelector('.button')
-var popup = document.querySelector('.hotel-class')
-
-byButton.addEventListener('click', function () {
-  popup.classList.add('hotel-none')
-})
-
-byButton.addEventListener('click', function () {
-    popup.classList.remove('hotel-none')
-  })
-  */
